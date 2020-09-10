b72cc7efd94b7415e0b65943d296fef8cf3a1913
Rashmi Bang <rashmibang06@gmail.com>
2020-09-10 22:23:19 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmailsComponent } from './emails/emails.component';
+import { EmaildataComponent } from './emaildata/emaildata.component';
+import { EditableComponent } from './editable/editable.component';
+
+const appRoutes: Routes = [
+    { path: 'emails', component: EmailsComponent },
+    { path: 'emails/:id', component: EmaildataComponent },
+    { path: 'emails/:id/mode', component: EditableComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        EmailsComponent,
+        EmaildataComponent,
+        EditableComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
